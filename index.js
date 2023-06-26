795b4e0e3b0bdcf9b9238ae37209ef9b3a8f188c
Jaden Falbee <j.falbee04@gmail.com>
2023-06-26 19:49:24 -0400

    finishes lab

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  if (feet <= 400) {
+    return "This one is on me!"
+  }
+  else if (400 < feet && feet < 2000) {
+    return "That will be twenty bucks."
+  }
+  else if (2000 < feet && feet < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else {
+    return"No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  switch (city) {
+    case 'NYC':
+      return "Ok, sounds good."
+      break;
+      default:
+        return "No go."
+        break;
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+      return "Thank you."
+      break;
+    default:
+      return "Bye."
+      break;
+  }
 }
+
